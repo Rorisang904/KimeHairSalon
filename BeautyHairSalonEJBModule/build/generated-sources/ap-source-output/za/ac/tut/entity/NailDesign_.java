6c1703245cebe85c791fee20c5ab375b771065f5
nkulebytheway <136344317+nkulebytheway@users.noreply.github.com>
2025-05-12 21:31:57 +0200

    Add files via upload

--- BeautyHairSalonEJBModule/build/generated-sources/ap-source-output/za/ac/tut/entity/NailDesign_.java
+++ BeautyHairSalonEJBModule/build/generated-sources/ap-source-output/za/ac/tut/entity/NailDesign_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-01T02:11:57")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-12T16:59:17")
 @StaticMetamodel(NailDesign.class)
 public class NailDesign_ { 
 
